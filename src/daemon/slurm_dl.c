56beb9636351a6a9fd0d199fe8ed9ecb0fe2c8b7
Andrew <andrewg@cray.com>
2016-02-08 08:51:07 -0600

    Added missing include headers

--- src/daemon/slurm_dl.c
+++ src/daemon/slurm_dl.c
@@ -20,8 +20,11 @@
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "cti_daemon.h"
 
