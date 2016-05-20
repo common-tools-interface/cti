09d73ffa2880705cdb4e1162071bcfce4b8f0bdc
kdavis <kdavis@cray.com>
2016-05-20 17:54:52 -0500

    Subject: Modified build.sh for buildbot Author: kdavis Description: Modifield build.sh for buildbot Bugs Affected: Bug Action:

--- build.sh
+++ build.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
 #1
 module purge;
-DATE=`date +%s`
-DATE=`date -d @$DATE "+%Y-%m-%d"`
-if [[ $1 == "-b" && ! -z $2 ]]
+#DATE=`date +%s`
+#DATE=`date -d @$DATE "+%Y-%m-%d"`
+if [[ $1 == "-d" && ! -z $2 ]]
 then
-  BUILD_DIR=$2/$DATE
+  BUILD_DIR=$2
 else
   BUILD_DIR=$PWD/install
+  if [ -d $BUILD_DIR ]
+  then
+    rm -rf $BUILD_DIR
+  fi
+  mkdir $BUILD_DIR
+  chmod a+rwx $BUILD_DIR
+  #mkdir $BUILD_DIR/install
+  #chmod a+rwx $BUILD_DIR/install
 fi
 echo "Building to " $BUILD_DIR
 
-if [ -d $BUILD_DIR ]
-then
-  rm -rf $BUILD_DIR
-fi
-mkdir $BUILD_DIR
-chmod a+rwx $BUILD_DIR
-mkdir $BUILD_DIR/install
-chmod a+rwx $BUILD_DIR/install
-
-./configure --prefix=$BUILD_DIR/install --with-gdb;
+./configure --prefix=$BUILD_DIR --with-gdb;
 make;
 make install
 make tests;
 
-mkdir $BUILD_DIR/install/docs
-cp $PWD/extras/docs/ATTRIBUTIONS_cti.txt $BUILD_DIR/install/docs
+mkdir $BUILD_DIR/docs
+cp $PWD/extras/docs/ATTRIBUTIONS_cti.txt $BUILD_DIR/docs
 
-rm -f $BUILD_DIR/install/lib/pkgconfig/*
-cp $PWD/scripts/craytools_be.pc  $BUILD_DIR/install/lib/pkgconfig/craytools_be.pc
-cp $PWD/scripts/craytools_fe.pc  $BUILD_DIR/install/lib/pkgconfig/craytools_fe.pc
-chmod a+rwx $BUILD_DIR/install/lib/pkgconfig/craytools_be.pc
-chmod a+rwx $BUILD_DIR/install/lib/pkgconfig/craytools_fe.pc
-#cd $2
-#ln -s $DATE latest
+rm -f $BUILD_DIR/lib/pkgconfig/*
+cp $PWD/scripts/craytools_be.pc  $BUILD_DIR/lib/pkgconfig/craytools_be.pc
+cp $PWD/scripts/craytools_fe.pc  $BUILD_DIR/lib/pkgconfig/craytools_fe.pc
+chmod a+rwx $BUILD_DIR/lib/pkgconfig/craytools_be.pc
+chmod a+rwx $BUILD_DIR/lib/pkgconfig/craytools_fe.pc
 
