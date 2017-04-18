63c429ef7f0d91793dc4c6faf83d088e68dcd92a
Karen Davis <kdavis@os-172-30-197-66.us.cray.com>
2017-04-18 15:17:24 -0500

    Subject: updated cti spec file Author: kdavis Description: updated spec file to specify what this cti package provides. This allows lgdb package to assess the cti name and version to determine if its a version of 1.0.5 or newer.

--- scripts/cti_spec.py
+++ scripts/cti_spec.py
@@ -18,7 +18,7 @@ URL: [url]
 Source: [tarball]
 BuildRoot: %{_tmppath}/%{name}-%{release}-root
 Prefix: /opt/cray/pe
-
+Provides: cray-cti = %{build_version}
 
 %define _use_internal_dependency_generator 0
 %define __find_requires %{scripts_d}/find-requires
