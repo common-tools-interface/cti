597c155611e675c0c0f82be4d764779780f0dee4
Andrew D'Angelo <adangelo@cray.com>
2018-07-19 11:38:40 -0500

    ignore more temporary files

--- external/libssh/build/doc/CMakeFiles/doc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "doc/html"
-  "doc/latex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- external/libssh/build/libssh-build-tree-settings.cmake
+++
@@ -1,1 +0,0 @@
-set(LIBSSH_INLUDE_DIR /cray/css/users/azahdeh/projects/cti_sles/external/libssh/include)
