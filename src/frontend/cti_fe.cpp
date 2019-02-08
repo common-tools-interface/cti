5ebba27535928dc88773151610b47f4b71138bc6
Andrew D'Angelo <adangelo@cray.com>
2019-02-08 16:00:28 -0600

    combine cti_run into cti_fe

--- src/frontend/cti_fe.cpp
+++ src/frontend/cti_fe.cpp
@@ -22,6 +22,7 @@
 
 #include <errno.h>
 #include <dlfcn.h>
+#include <fcntl.h>
 #include <pwd.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -36,6 +37,7 @@
 #include <memory>
 
 #include "cti_fe.h"
+#include "cti_run.h"
 #include "cti_defs.h"
 #include "cti_error.h"
 
@@ -382,6 +384,7 @@ cti_wlm_type_toString(cti_wlm_type wlm_type) {
 
 template <typename FuncType, typename ReturnType = decltype(std::declval<FuncType>()())>
 static ReturnType runSafely(std::string const& caller, FuncType func) {
+	std::cerr << caller << std::endl;
 	try {
 		return func();
 	} catch (std::exception const& ex) {
@@ -574,3 +577,104 @@ cti_app_id_t cti_ssh_registerJob(pid_t launcher_pid) {
 	return 0;
 #endif
 }
+
+/* app launch / release functions */
+
+static int
+_cti_checkofd(int fd)
+{
+	int flags;
+
+	// if fd is -1, then the fd arg is meant to be ignored
+	if (fd == -1) {
+		return 0;
+	}
+	errno = 0;
+	flags = fcntl(fd, F_GETFL);
+	if (errno != 0) {
+		return 1;
+	}
+	flags &= O_ACCMODE;
+	if ((flags & O_WRONLY) || (flags & O_RDWR)) {
+		return 0;
+	}
+	return 1;
+}
+
+// This does sanity checking on args in common for both launchApp and launchAppBarrier
+static void
+_cti_checkLaunchArgs(int stdout_fd, int stderr_fd, const char *inputFile, const char *chdirPath)
+{
+	// check stdout, stderr
+	if (_cti_checkofd(stdout_fd)) {
+		throw std::runtime_error("Invalid stdout_fd argument.");
+	}
+	if (_cti_checkofd(stderr_fd)) {
+		throw std::runtime_error("Invalid stderr_fd argument.");
+	}
+
+	// verify inputfile is good
+	if (inputFile != nullptr) {
+		struct stat st;
+		if (stat(inputFile, &st)) { // make sure inputfile exists
+			throw std::runtime_error("Invalid inputFile argument. File does not exist.");
+		}
+		if (!S_ISREG(st.st_mode)) { // make sure it is a regular file
+			throw std::runtime_error("Invalid inputFile argument. The file is not a regular file.");
+		}
+		if (access(inputFile, R_OK)) { // make sure we can access it
+			throw std::runtime_error("Invalid inputFile argument. Bad permissions.");
+		}
+	}
+
+	// verify chdirpath is good
+	if (chdirPath != nullptr) {
+		struct stat st;
+		if (stat(chdirPath, &st)) { // make sure chdirpath exists
+			throw std::runtime_error("Invalid chdirPath argument. Directory does not exist.");
+		}
+		if (!S_ISDIR(st.st_mode)) { // make sure it is a directory
+			throw std::runtime_error("Invalid chdirPath argument. The file is not a directory.");
+		}
+		if (access(chdirPath, R_OK | W_OK | X_OK)) { // make sure we can access it
+			throw std::runtime_error("Invalid chdirPath argument. Bad permissions.");
+		}
+	}
+}
+
+cti_app_id_t
+cti_launchApp(const char * const launcher_argv[], int stdout_fd, int stderr_fd,
+	const char *inputFile, const char *chdirPath, const char * const env_list[])
+{
+	return runSafely("cti_launchApp", [&](){
+		_cti_checkLaunchArgs(stdout_fd, stderr_fd, inputFile, chdirPath);
+		return _cti_getCurrentFrontend().launch(launcher_argv, stdout_fd, stderr_fd, inputFile, chdirPath, env_list);
+	});
+}
+
+cti_app_id_t
+cti_launchAppBarrier(const char * const launcher_argv[], int stdout_fd, int stderr_fd,
+	const char *inputFile, const char *chdirPath, const char * const env_list[])
+{
+	return runSafely("cti_launchAppBarrier", [&](){
+		_cti_checkLaunchArgs(stdout_fd, stderr_fd, inputFile, chdirPath);
+		return _cti_getCurrentFrontend().launchBarrier(launcher_argv, stdout_fd, stderr_fd, inputFile, chdirPath, env_list);
+	});
+}
+
+int
+cti_releaseAppBarrier(cti_app_id_t appId) {
+	return !runSafely("cti_releaseAppBarrier", [&](){
+		_cti_getCurrentFrontend().getApp(appId).releaseBarrier();
+		return 1;
+	});
+}
+
+int
+cti_killApp(cti_app_id_t appId, int signum) {
+	return !runSafely("cti_killApp", [&](){
+		_cti_getCurrentFrontend().getApp(appId).kill(signum);
+		return 1;
+	});
+}
+
