d5cd0ac4413e53e4ca97c97fdc5cb6f99bc71b0e
Andrew D'Angelo <adangelo@cray.com>
2019-03-22 12:29:10 -0500

    test running tool daemons with same library name, different library behavior

--- tests/unit/cti_fe_iface_test.cpp
+++ tests/unit/cti_fe_iface_test.cpp
@@ -244,6 +244,28 @@ TEST_F(CTIFEIfaceTest, CreateManifest) {
 	EXPECT_EQ(cti_releaseAppBarrier(appId), SUCCESS);
 }
 
+static void
+testPrintingDaemon(cti_session_id_t sessionId, char const* daemonPath, std::string const& expecting)
+{
+	// create manifest
+	auto const manifestId = cti_createManifest(sessionId);
+	ASSERT_EQ(cti_manifestIsValid(manifestId), true);
+
+	// set up output file
+	auto const outputPath = temp_file_handle{};
+	char const* toolDaemonArgs[] = {outputPath.get(), nullptr};
+	ASSERT_EQ(cti_execToolDaemon(manifestId, daemonPath, toolDaemonArgs, nullptr), SUCCESS);
+	sleep(1); // let tool daemon run
+
+	// read output file
+	{ std::ifstream outputFile(outputPath.get());
+		ASSERT_TRUE(outputFile.is_open());
+		std::string line;
+		ASSERT_TRUE(std::getline(outputFile, line));
+		EXPECT_EQ(line, expecting);
+	}
+}
+
 // Test that an app can run a tool daemon
 TEST_F(CTIFEIfaceTest, ExecToolDaemon) {
 	// set up app
@@ -263,23 +285,36 @@ TEST_F(CTIFEIfaceTest, ExecToolDaemon) {
 	auto const sessionId = cti_createSession(appId);
 	ASSERT_EQ(cti_sessionIsValid(sessionId), true);
 
-	// create manifest
-	auto const manifestId = cti_createManifest(sessionId);
-	ASSERT_EQ(cti_manifestIsValid(manifestId), true);
+	// run printing daemons
+	testPrintingDaemon(sessionId, "./stage_test/one_printer", "1");
 
-	// set up output file
-	auto const outputPath = temp_file_handle{};
-	char const* toolDaemonArgs[] = {outputPath.get(), nullptr};
-	ASSERT_EQ(cti_execToolDaemon(manifestId, "./stage_test/one_printer", toolDaemonArgs, nullptr), SUCCESS);
-	sleep(1); // let tool daemon run
+	// cleanup
+	EXPECT_EQ(cti_destroySession(sessionId), SUCCESS);
+	EXPECT_EQ(cti_releaseAppBarrier(appId), SUCCESS);
+}
 
-	// read output file
-	{ std::ifstream outputFile(outputPath.get());
-		ASSERT_TRUE(outputFile.is_open());
-		std::string line;
-		ASSERT_TRUE(std::getline(outputFile, line));
-		EXPECT_EQ(line, "1");
-	}
+// Test that an app can launch two tool daemons using different libraries with the same name
+TEST_F(CTIFEIfaceTest, DaemonLibDir) {
+	// set up app
+	char const* argv[] = {"/bin/sh", nullptr};
+	auto const  stdoutFd = -1;
+	auto const  stderrFd = -1;
+	char const* inputFile = nullptr;
+	char const* chdirPath = nullptr;
+	char const* const* envList  = nullptr;
+
+	// create app
+	auto const appId = watchApp(cti_launchAppBarrier(argv, stdoutFd, stderrFd, inputFile, chdirPath, envList));
+	ASSERT_GT(appId, 0);
+	EXPECT_EQ(cti_appIsValid(appId), true);
+
+	// create app's session
+	auto const sessionId = cti_createSession(appId);
+	ASSERT_EQ(cti_sessionIsValid(sessionId), true);
+
+	// run printing daemons
+	testPrintingDaemon(sessionId, "./stage_test/one_printer", "1");
+	testPrintingDaemon(sessionId, "./stage_test/two_printer", "2");
 
 	// cleanup
 	EXPECT_EQ(cti_destroySession(sessionId), SUCCESS);
