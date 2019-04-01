702e17e60b41f25fd39e513137290f1ef2dbfe2e
Andrew D'Angelo <adangelo@cray.com>
2019-04-01 12:59:59 -0500

    add copyright header to avocado tests

--- tests/function/avocado_tests.py
+++ tests/function/avocado_tests.py
@@ -1,3 +1,13 @@
+'''
+ * Copyright 2019 Cray Inc.  All Rights Reserved.
+ *
+ * Unpublished Proprietary Information.
+ * This unpublished work is protected to trade secret, copyright and other laws.
+ * Except as permitted by contract or express written permission of Cray Inc.,
+ * no part of this work or its content may be used, reproduced or disclosed
+ * in any form.
+'''
+
 from avocado import Test
 from avocado.utils import process
 
