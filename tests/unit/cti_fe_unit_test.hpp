996611fe79c7b80c300687d0b4f55b6a3f739a19
Andrew Gontarek <andrewg@cray.com>
2019-04-02 14:14:02 -0500

    Add unit test fixes
    Ensure we setup the environment variable for install path correctly.

--- tests/unit/cti_fe_unit_test.hpp
+++ tests/unit/cti_fe_unit_test.hpp
@@ -1,3 +1,16 @@
+/******************************************************************************\
+ * cti_fe_unit_test.hpp - Frontend unit tests for CTI
+ *
+ * Copyright 2019 Cray Inc.  All Rights Reserved.
+ *
+ * Unpublished Proprietary Information.
+ * This unpublished work is protected to trade secret, copyright and other laws.
+ * Except as permitted by contract or express written permission of Cray Inc.,
+ * no part of this work or its content may be used, reproduced or disclosed
+ * in any form.
+ *
+ ******************************************************************************/
+
 #pragma once
 
 #include "frontend/cti_fe_iface.h"
