da813b3231c29fdd59b2837b5074bd629adda35b
Karen Glaser <kglaser@pe03.us.cray.com>
2019-06-03 10:57:37 -0500

    Subject: updated CTI example test case Author: kglaser Description: updated to support ssh wlm and remove support for alps.

--- examples/cti_transfer_example.c
+++ examples/cti_transfer_example.c
@@ -1,15 +1,22 @@
 /*********************************************************************************\
- * cti_transfer_example.c - An example program which takes advantage of the Cray
+ * cti_transfer_test.c - An example program which takes advantage of the Cray
  *			tools interface which will launch an application session from the
  *			given argv and transfer test files.
  *
- * Copyright 2011-2016 Cray Inc.  All Rights Reserved.
+ * Copyright 2011-2019 Cray Inc.  All Rights Reserved.
+ *
+ * Unpublished Proprietary Information.
+ * This unpublished work is protected to trade secret, copyright and other laws.
+ * Except as permitted by contract or express written permission of Cray Inc.,
+ * no part of this work or its content may be used, reproduced or disclosed
+ * in any form.
  *
  *********************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "cray_tools_fe.h"
 
@@ -31,170 +38,139 @@ main(int argc, char **argv)
 	char *				file_loc;
 	cti_wlm_type		mywlm;
 	int					r;
-	
-	if (argc < 2)
-	{
+
+	if (argc < 2) {
 		usage(argv[0]);
+		assert(argc > 2);
 		return 1;
 	}
-	
+
 	/*
 	 * cti_launchAppBarrier - Start an application using the application launcher
 	 *                        with the provided argv array and have the launcher
-	 *                        hold the application at its startup barrier for 
+	 *                        hold the application at its startup barrier for
 	 *                        MPI/SHMEM/UPC/CAF applications.
 	 */
 	myapp = cti_launchAppBarrier((const char * const *)&argv[1],-1,-1,NULL,NULL,NULL);
-	if (myapp == 0)
-	{
+	if (myapp == 0) {
 		fprintf(stderr, "Error: cti_launchAppBarrier failed!\n");
 		fprintf(stderr, "CTI error: %s\n", cti_error_str());
-		return 1;
 	}
-	
+	assert(myapp != 0);
+
 	// Ensure app is valid
-	if(cti_appIsValid(myapp) == 0){
-		fprintf(stderr, "Error: app is invalid!\n");
-		return 1;
-	}
-	
+	assert(cti_appIsValid(myapp) != 0);
+
 	// Create a new session based on the app_id
 	mysid = cti_createSession(myapp);
-	if (mysid == 0)
-	{
+	if (mysid == 0) {
 		fprintf(stderr, "Error: cti_createSession failed!\n");
 		fprintf(stderr, "CTI error: %s\n", cti_error_str());
-		return 1;
 	}
-	
+	assert(mysid != 0);
+
 	// Ensure session is valid
-	if(cti_sessionIsValid(mysid) == 0){
-		fprintf(stderr, "Error: session is invalid!\n");
-		return 1;
-	}
-	
+	assert(cti_sessionIsValid(mysid) != 0);
+
 	// Create a manifest based on the session
 	mymid = cti_createManifest(mysid);
-	if (mymid == 0)
-	{
+	if (mymid == 0) {
 		fprintf(stderr, "Error: cti_createManifest failed!\n");
 		fprintf(stderr, "CTI error: %s\n", cti_error_str());
-		return 1;
 	}
-	
+	assert(mymid != 0);
+
 	// Ensure manifest is valid
-	if(cti_manifestIsValid(mymid) == 0){
-		fprintf(stderr, "Error: manifest is invalid!\n");
-		return 1;
-	}
-	
+	assert(cti_manifestIsValid(mymid) != 0);
+
 	// Add the file to the manifest
 	r = cti_addManifestFile(mymid, "testing.info");
-	if (r)
-	{
+	if (r) {
 		fprintf(stderr, "Error: cti_addManifestFile failed!\n");
 		fprintf(stderr, "CTI error: %s\n", cti_error_str());
-		return 1;
 	}
-	
+	assert(r == 0);
+
+	// Ensure manifest is valid
+	assert(cti_manifestIsValid(mymid) != 0);
+
 	// Send the manifest to the compute node
 	r = cti_sendManifest(mymid);
-	if (r)
-	{
+	if (r) {
 		fprintf(stderr, "Error: cti_sendManifest failed!\n");
 		fprintf(stderr, "CTI error: %s\n", cti_error_str());
-		return 1;
 	}
-	
+	assert(r == 0);
+
+	// Ensure manifest is no longer valid
+	assert(cti_manifestIsValid(mymid) == 0);
+
 	// Get the location of the directory where the file now resides on the
 	// compute node
 	file_loc = cti_getSessionFileDir(mysid);
-	if (file_loc == NULL)
-	{
+	if (file_loc == NULL) {
 		fprintf(stderr, "Error: cti_getSessionFileDir failed!\n");
 		fprintf(stderr, "CTI error: %s\n", cti_error_str());
-		return 1;
 	}
-	
+	assert(file_loc != NULL);
+
 	printf("Sent testing.info to the directory %s on the compute node(s).\n", file_loc);
-	
+
 	// Get the current WLM in use so that we can verify based on that
 	mywlm = cti_current_wlm();
-	if(mywlm == CTI_WLM_NONE){
-		fprintf(stderr, "Error: Could not succesfully detect workload manager!\n");
-		return 1;
-	}
-	
+	assert(mywlm != CTI_WLM_NONE);
+
 	// Conduct WLM specific calls
-	switch (mywlm)
-	{
-		case CTI_WLM_ALPS:
-		{
-			cti_aprunProc_t *	myapruninfo;
-			
-			/*
-			 * cti_getAprunInfo - Obtain information about the aprun process
-			 */
-			myapruninfo = cti_alps_getAprunInfo(myapp);
-			if (myapruninfo == NULL)
-			{
-				fprintf(stderr, "Error: cti_alps_getAprunInfo failed!\n");
-				fprintf(stderr, "CTI error: %s\n", cti_error_str());
-				return 1;
-			} else
-			{
-				printf("\nVerify by issuing the following commands in another terminal:\n\n");
-				printf("module load nodehealth\n");
-				printf("pcmd -a %llu \"ls %s\"\n", (long long unsigned int)myapruninfo->apid, file_loc);
-				free(myapruninfo);
-			}
-		}
-			break;
-			
+	switch (mywlm) {
 		case CTI_WLM_CRAY_SLURM:
+        case CTI_WLM_SLURM:
+        case CTI_WLM_SSH:
 		{
 			cti_srunProc_t *	mysruninfo;
-			
+
 			/*
 			 * cti_cray_slurm_getSrunInfo - Obtain information about the srun process
 			 */
 			 mysruninfo = cti_cray_slurm_getSrunInfo(myapp);
-			 if (mysruninfo == NULL)
-			 {
+			 if (mysruninfo == NULL) {
 			 	fprintf(stderr, "Error: cti_cray_slurm_getSrunInfo failed!\n");
 				fprintf(stderr, "CTI error: %s\n", cti_error_str());
-				return 1;
-			 } else
-			 {
+			 }
+			 else {
 			 	printf("\nVerify by issuing the following commands in another terminal:\n\n");
 			 	printf("srun --jobid=%lu --gres=none --mem-per-cpu=0 ls %s\n", (long unsigned int)mysruninfo->jobid, file_loc);
-				free(mysruninfo);
 			 }
+			 assert(mysruninfo != NULL);
+			 free(mysruninfo);
 		}
 			break;
-		
+
 		default:
 			// do nothing
 			printf("Unsupported wlm!\n");
+			assert(0);
 			break;
 	}
-	
+
 	free(file_loc);
-	
+
 	printf("\nHit return to release the application from the startup barrier...");
-	
+
 	// just read a single character from stdin then release the app/exit
 	(void)getchar();
-	
+
 	r = cti_releaseAppBarrier(myapp);
-	if (r)
-	{
+	if (r) {
 		fprintf(stderr, "Error: cti_releaseAppBarrier failed!\n");
 		fprintf(stderr, "CTI error: %s\n", cti_error_str());
-		return 1;
 	}
-	
+	assert(r == 0);
+
 	cti_deregisterApp(myapp);
-	
+
+	// ensure deregister worked.
+	assert(cti_sessionIsValid(mysid) == 0);
+	assert(cti_appIsValid(myapp) == 0);
+
 	return 0;
 }
