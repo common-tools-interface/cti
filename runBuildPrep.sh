b5ce8c975ac2ce6561896b0b71735ffe6865c822
Karen Glaser <kglaser@pe03.us.cray.com>
2019-06-17 10:38:18 -0500

    Subject: updated buildprep script Author: kglaser Description: Utilizing zypper to install the latest dependent rpms. Updated the module use path. Provided error handling for zypper installs.
    Subject: updated buildprep
    Author: kglaser
    Description: changed zypper install error handling function name,
    and directly sending return value of zypper install as a parameter
    to the function.
    
    Subject: updated buildprep
    Author: kglaser
    Description: adding echo statment back in.

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -14,77 +14,60 @@
 # TODO: Once we move away from monolithic builds, point at the proper repositories
 # ./add_rpm_repo.sh http://car.dev.cray.com/artifactory/internal/PE-CDST/ /x86_64/dev/master/
 
-# Install the rpms in an OS agnostic fashion using DST scripts
-#./install_rpm.sh cmake
-# FIXME: install_rpm.sh not supported in pe pipeline
+gcc_ver=6.1.0
+
+function check_exit_status(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo "There was an error installing $2: $return_value"
+        exit $return_value
+    fi
+}
 
 echo "############################################"
 echo "#             Installing deps              #"
 echo "############################################"
 zypper --non-interactive --no-gpg-check install cmake
+check_exit_status $? cmake
+
 zypper --non-interactive --no-gpg-check install flex
+check_exit_status $? flex
+
 zypper --non-interactive --no-gpg-check install bison
+check_exit_status $? bison
+
 zypper --non-interactive --no-gpg-check install binutils-devel
+check_exit_status $? binutils-devel
+
 zypper --non-interactive --no-gpg-check install mksh
+check_exit_status $? mksh
+    
 zypper --non-interactive --no-gpg-check install tcl
+check_exit_status $? tcl
+
+zypper addrepo http://car.dev.cray.com/artifactory/shasta-premium/SHASTA-OS/sle15_premium/x86_64/cray/sles15-premium/ CAR
+check_exit_status $? CAR
+
+zypper --non-interactive --no-gpg-check install craype
+check_exit_status $? craype
+
+zypper --non-interactive --no-gpg-check install cray-set-gcc-libs
+check_exit_status $? cray-set-gcc-libs
+
+zypper --non-interactive --no-gpg-check install cray-gcc-$gcc_ver
+check_exit_status $? cray-gcc-$gcc_ver
+
+zypper --non-interactive --no-gpg-check install cray-modules
+check_exit_status $? cray-modules
 
 export SHELL=/bin/sh
 
-#Download cray-gcc-6.1.0 & cray-modules from artifactory if system does not have it installed.
-artifactory_repo="http://car.dev.cray.com/artifactory"
-shasta_repo="shasta-premium/SHASTA-OS/sle15_premium/x86_64/cray/sles15-premium/kernel-team/Cray-shasta-compute-sles15-1.0.149-20190612161557-gd8b9d40.rpmdir"
-
-gcc_pkg="cray-gcc-6.1.0-201811010911.2fb57b1fae09e-343.sles15.x86_64.rpm"
-gcc_url="$artifactory_repo/$shasta_repo/$gcc_pkg"
-
-#gcclibs and craype are dependencies of gcc
-gcclibs_pkg="cray-set-gcc-libs-1.0.4-201702220827.c43e1964399fe-12.x86_64.rpm"
-gcclibs_url="$artifactory_repo/$shasta_repo/$gcclibs_pkg"
-
-craype_pkg="craype-2.5.19.5-201903131827.0acb5ac2283e3-1.sles15.x86_64.rpm"
-craype_url="$artifactory_repo/$shasta_repo/$craype_pkg"
-
-modules_pkg="cray-modules-3.2.11.1-2S.201901112058.67ac7225c9b34.sles12.x86_64.rpm"
-modules_url="$artifactory_repo/$shasta_repo/$modules_pkg"
-
-
-queryStr=`echo $gcc_pkg | cut -d'.' -f1-6`
-gcc_installed=`rpm -qa | grep $queryStr`
-if [ -z $gcc_installed ]
-then
-  curl -v -L -o $gcclibs_pkg $gcclibs_url
-  curl -v -L -o $craype_pkg $craype_url 
-  curl -v -L -o $gcc_pkg $gcc_url
-  rpm -ivh $PWD/$craype_pkg $PWD/$gcclibs_pkg $PWD/$gcc_pkg
-  return_value=$?
-  if [ $return_value -eq 1 ]
-  then
-    echo "There was an error installing $gcc_pkg"
-    exit $return_value
-  fi
-
-fi
-
-#get the modules package and install
-queryStr="cray-modules"
-modules_installed=`rpm -qa | grep $queryStr | head -1`
-if [ -z $modules_installed ]
-then
-  curl -v -L -o $modules_pkg $modules_url
-  rpm -ivh $PWD/$modules_pkg
-  return_value=$?
-  if [ $return_value -eq 1 ]
-  then
-    echo "There was an error installing $modules_pkg"
-    exit $return_value
-  fi
-fi
-
 #Ensure we can use modules
-module use /opt/modulefiles
+module use /opt/cray/pe/modules/default/init/bash
 
-#Ensure CTI is build with gcc/6.1.0
-module load gcc/6.1.0
+#Ensure CTI is build with $gcc_ver
+module load gcc/$gcc_ver
 
 echo "############################################"
 echo "#      Capturing Jenkins Env Vars          #"
@@ -100,6 +83,8 @@ else
   echo "Unable to determine Jenkins BUILD_NUMBER"
   exit 1
 fi
+
+
 
 echo "############################################"
 echo "#      Generating configure files          #"
