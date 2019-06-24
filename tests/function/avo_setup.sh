09e5c175dc0a44f5442930c47f9138185c017e0b
Aaron Duebner <aduebner@cray.com>
2019-06-24 14:01:02 -0500

    Avocado build and run scripts now fail better

--- tests/function/avo_setup.sh
+++ tests/function/avo_setup.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
 #Create avocado environment
-mkdir avocado-virtual-environment
-cd avocado-virtual-environment
-python3 -m venv avocado
+if mkdir avocado-virtual-environment && cd avocado-virtual-environment ; then
+    if python3 -m venv avocado ; then
 
-#Install avocado plugins
-. $PWD/avocado/bin/activate
-pip install avocado-framework
-#pip install avocado-framework-plugin-result-html
-pip install avocado-framework-plugin-loader-yaml
-pip install avocado-framework-plugin-varianter-yaml-to-mux
+        #Install avocado plugin
+        if . $PWD/avocado/bin/activate ; then
+            if pip install avocado-framework ; then
 
-#Configure avocado
-mkdir job-results
-PYTHON_VERSION="$(ls $PWD/avocado/lib/)" 
-python3 ../avo_config.py $PWD $PYTHON_VERSION
+            #Install additional avocado plugins
+            #pip install avocado-framework-plugin-loader-yaml
+            #pip install avocado-framework-plugin-varianter-yaml-to-mux
+
+            #Configure avocado
+                if mkdir job-results ; then
+                    PYTHON_VERSION="$(ls $PWD/avocado/lib/)" 
+                    python3 ../avo_config.py $PWD $PYTHON_VERSION
+                else
+                    echo "Failed to create job-results directory"
+                    echo "Job-results will now be stored in ~/avocado"
+                fi
+            else
+                echo "Pip failed to install avocado-framework"
+                echo "Cleaning up..."
+	        cd ../
+                rm -r avocado-virtual-environment
+            fi
+        else
+            echo "Failed to activate python virtual environment"
+            echo "Cleaning up..."
+            cd ../
+            rm -r avocado-virtual-environment
+        fi
+    else
+        echo "Failed to create python virtual environment"
+        echo "Cleaning up..."
+        cd ../
+        rm -r avocado-virtual-environment
+    fi
+else
+    echo "Failed to create avocado-virtual-environment directory"
+fi
--- tests/function/build_run.sh
+++ tests/function/build_run.sh
@@ -2,8 +2,12 @@
 chmod +x ./avo_setup.sh
 chmod +x ./launch_functional_test.sh
 if ./avo_setup.sh ; then
-    ./avocado-virtual-environment/avocado/bin/avocado run ./whitebox_avocado_tests.py
+    if ssh $HOSTNAME exit ; then #If MPI apps can run
+        ./avocado-virtual-environment/avocado/bin/avocado run ./avocado_tests.py
+    else
+        echo "SSH not properly setup to allow for testing: TODO: JUST FIX THAT HERE"
+    fi
 else
-    echo "Failed to setup avocado environment"
+    echo "Failed to setup avocado environment."
 fi
 
