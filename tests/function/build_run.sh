c5a4b726e1006865e5b85066047780cd6ba14420
Aaron Duebner <aduebner@cray.com>
2019-06-24 15:11:26 -0500

    Created script to check for vaild ssh setup + build and run now checks for proper ssh before execution of tests

--- tests/function/build_run.sh
+++ tests/function/build_run.sh
@@ -2,12 +2,19 @@
 chmod +x ./avo_setup.sh
 chmod +x ./launch_functional_test.sh
 if ./avo_setup.sh ; then
-    if ssh $HOSTNAME exit ; then #If MPI apps can run
+    #If mpi apps can run
+    if ./ssh_valid.sh ; then
+        export MPIEXEC_TIMEOUT=10
+        export CRAY_CTI_DIR=$PWD/../../install
+        export CRAY_CTI_LAUNCHER_NAME=/opt/cray/pe/snplauncher/default/bin/mpiexec
+        export CRAY_CTI_WLM=generic
         ./avocado-virtual-environment/avocado/bin/avocado run ./avocado_tests.py
     else
-        echo "SSH not properly setup to allow for testing: TODO: JUST FIX THAT HERE"
+        echo "SSH not properly setup to allow for testing."
+        exit 1
     fi
 else
     echo "Failed to setup avocado environment."
+    exit 1
 fi
 
