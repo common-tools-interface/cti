bf7a16062108bcaf8b3b27053aeb352e9c458563
Aaron Duebner <aduebner@cray.com>
2019-06-24 15:24:10 -0500

    New script to run all tests + build_run now uses script + launch_functional takes additional requirement

--- tests/function/build_run.sh
+++ tests/function/build_run.sh
@@ -3,12 +3,8 @@ chmod +x ./avo_setup.sh
 chmod +x ./launch_functional_test.sh
 if ./avo_setup.sh ; then
     #If mpi apps can run
-    if ./ssh_valid.sh ; then
-        export MPIEXEC_TIMEOUT=10
-        export CRAY_CTI_DIR=$PWD/../../install
-        export CRAY_CTI_LAUNCHER_NAME=/opt/cray/pe/snplauncher/default/bin/mpiexec
-        export CRAY_CTI_WLM=generic
-        ./avocado-virtual-environment/avocado/bin/avocado run ./avocado_tests.py
+    if ./valid_ssh.sh ; then
+        ./run_tests.sh
     else
         echo "SSH not properly setup to allow for testing."
         exit 1
--- tests/function/launch_functional_test.sh
+++ tests/function/launch_functional_test.sh
@@ -3,5 +3,6 @@ if [ "$#" -lt 1 ]; then
     echo "Illegal number of arguments"
     echo "Expected at least 1 executable with optional parameters"
 else
-    MPIEXEC_TIMEOUT=10 CRAY_CTI_DIR=$PWD/../../install CRAY_CTI_LAUNCHER_NAME=/opt/cray/pe/snplauncher/default/bin/mpiexec CRAY_CTI_WLM=generic "$@"
+    module load cray-snplauncher
+    MPICH_SMP_SINGLE_COPY_OFF=0 MPIEXEC_TIMEOUT=10 CRAY_CTI_DIR=$PWD/../../install CRAY_CTI_LAUNCHER_NAME=/opt/cray/pe/snplauncher/default/bin/mpiexec CRAY_CTI_WLM=generic "$@"
 fi
