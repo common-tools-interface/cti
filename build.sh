6655512fd5f4a300630dcb77588e15285a2d0241
Aaron Duebner <aduebner@cray.com>
2019-07-01 11:37:47 -0500

    Functional tests no longer run automatically as part of the main build process

--- build.sh
+++ build.sh
@@ -99,6 +99,4 @@ cp $PWD/craytools_fe.pc  $BUILD_DIR/lib/pkgconfig/craytools_fe.pc
 chmod a+rwx $BUILD_DIR/lib/pkgconfig/craytools_be.pc
 chmod a+rwx $BUILD_DIR/lib/pkgconfig/craytools_fe.pc
 
-./tests/function/build_run.sh ./tests/function/
-
 echo "Done"
