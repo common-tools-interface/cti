fc9246575bf7ba90b509ba2b72051c4e189deeb9
Aaron Duebner <aduebner@cray.com>
2019-07-30 15:38:40 -0500

    Some work on cti_execvp test

--- tests/unit/cti_useful_unit_test.cpp
+++ tests/unit/cti_useful_unit_test.cpp
@@ -38,6 +38,7 @@
 #include <unordered_set>
 #include <fstream>
 #include <algorithm>
+#include <fcntl.h>
 
 #include "cti_useful_unit_test.hpp"
 
@@ -57,61 +58,110 @@ CTIUsefulUnitTest::~CTIUsefulUnitTest()
 // test that
 TEST_F(CTIUsefulUnitTest, cti_argv)
 { 
-    // Begin ManagedArgv tests
-    cti::ManagedArgv argv1;
+    {
+        // Begin ManagedArgv tests
+        cti::ManagedArgv argv1;
 
-    // test that an argv with no data is of size 0
-    ASSERT_EQ(argv1.size(), 1);
-    char** argv_data = nullptr;
+        // test that an argv with no data is of size 0
+        ASSERT_EQ(argv1.size(), 1);
+        char** argv_data = nullptr;
 
-    // test that an argv with no data has a nullptr for its data 
-    argv_data = argv1.get();
-    ASSERT_EQ(argv_data[0], nullptr);
+        // test that an argv with no data has a nullptr for its data 
+        argv_data = argv1.get();
+        ASSERT_EQ(argv_data[0], nullptr);
 
-    // test that after adding a string these have changed as expected
-    std::string arg0 = "arg0";
-    argv1.add(arg0);
-    ASSERT_EQ(argv1.size(), 2);
+        // test that after adding a string these have changed as expected
+        std::string arg0 = "arg0";
+        argv1.add(arg0);
+        ASSERT_EQ(argv1.size(), 2);
 
-    // test that this new arg can be gotten using .get()
-    argv_data = argv1.get();
-    ASSERT_STREQ(argv_data[0], "arg0");
+        // test that this new arg can be gotten using .get()
+        argv_data = argv1.get();
+        ASSERT_STREQ(argv_data[0], "arg0");
 
-    // add a string via a character array and check for approritate results
-    char arg1[5] = "arg1";
-    arg1[4] = '\0';
-    argv1.add(arg1);
-    ASSERT_EQ(argv1.size(), 3);
+        // add a string via a character array and check for approritate results
+        char arg1[5] = "arg1";
+        arg1[4] = '\0';
+        argv1.add(arg1);
+        ASSERT_EQ(argv1.size(), 3);
  
-    // test that this data can be retrieved
-    argv_data = argv1.get();
-    ASSERT_STREQ(argv_data[1], "arg1");
+        // test that this data can be retrieved
+        argv_data = argv1.get();
+        ASSERT_STREQ(argv_data[1], "arg1");
 
-    // test that initializing a managedArgv with an initialization list works as expected
-    std::initializer_list<std::string const> strlist {"0","1","2","3","4"};
-    cti::ManagedArgv argv2(strlist);
-    ASSERT_EQ(argv2.size(), 6);
+        // test that initializing a managedArgv with an initialization list works as expected
+        std::initializer_list<std::string const> strlist {"0","1","2","3","4"};
+        cti::ManagedArgv argv2(strlist);
+        ASSERT_EQ(argv2.size(), 6);
     
-    // test that all data is present
-    char** argv_data2 = argv2.get();
-    for(int i = 0; i < 5; i++) {
-        ASSERT_STREQ(argv_data2[i], std::to_string(i).c_str());
+        // test that all data is present
+        char** argv_data2 = argv2.get();
+        for(int i = 0; i < 5; i++) {
+            ASSERT_STREQ(argv_data2[i], std::to_string(i).c_str());
+        }
     }
-
     /**********************************************************************************/
     // test additional argv classes
 }
+
 /*
 // test that
 TEST_F(CTIUsefulUnitTest, cti_dlopen)
 { 
+    
 }
+*/
 
 // test that
 TEST_F(CTIUsefulUnitTest, cti_execvp)
 { 
-}
+    {
+        // test fdbuf
+        ASSERT_THROW({
+            try {
+                cti::FdBuf buf_fail(-1);
+            } catch (const std::exception& ex) {
+                ASSERT_STREQ("Invalid file descriptor", ex.what());
+                throw;
+            }
+        }, std::invalid_argument);
 
+        int fd = open("testfile.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+        EXPECT_NO_THROW(cti::FdBuf buf(fd));
+        remove("testfile.txt");
+    }
+
+    {
+        // test FdPair
+
+        // test creation of a fdpair
+        ASSERT_NO_THROW(cti::FdPair fdp);
+
+        // test that close fd's behave as exxpected
+        cti::FdPair testfdp;
+        {
+            bool ex = 0;
+            try {
+                testfdp.closeRead();
+            } catch (...) {
+                ex = 1;
+            }
+            ASSERT_EQ(ex,1);
+        }
+
+        {
+            bool ex = 0;
+            try {
+                testfdp.closeWrite();
+            } catch (...) {
+                ex = 1;
+            }
+            ASSERT_EQ(ex,1);
+        }
+    }
+   
+}
+/*
 // test that
 TEST_F(CTIUsefulUnitTest, cti_log)
 { 
