42aa1069f571be0bf22267589c1503172d885d10
Jared Heuschele <jaredh@cray.com>
2019-09-23 14:05:13 +0000

    fix to build.sh to remove echo statements.

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #
+# A basic script to call the Jenkins scripts to build cti and all of its
+# dependencies.
+#
 # Copyright 2011-2019 Cray Inc. All Rights Reserved.
 #
-
-echo "build.sh start."
-
+# Unpublished Proprietary Information.
+# This unpublished work is protected to trade secret, copyright and other laws.
+# Except as permitted by contract or express written permission of Cray Inc.,
+# no part of this work or its content may be used, reproduced or disclosed
+# in any form.
 ./runBuildPrep.sh
 ./runBuild.sh
-
-echo "build.sh done."
