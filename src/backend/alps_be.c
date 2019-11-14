b328321419c1b11a01f9ff534ed3856e798da058
Andrew D'Angelo <adangelo@cray.com>
2019-11-14 09:51:49 -0600

    update ALPS component copyright information

--- src/backend/alps_be.c
+++ src/backend/alps_be.c
@@ -1,18 +1,13 @@
 /*********************************************************************************\
  * alps_be.c - alps specific backend library functions.
  *
- * Copyright 2014 Cray Inc.  All Rights Reserved.
+ * Copyright 2014-2019 Cray Inc.  All Rights Reserved.
  *
  * Unpublished Proprietary Information.
  * This unpublished work is protected to trade secret, copyright and other laws.
  * Except as permitted by contract or express written permission of Cray Inc.,
  * no part of this work or its content may be used, reproduced or disclosed
  * in any form.
- *
- * $HeadURL$
- * $Date$
- * $Rev$
- * $Author$
  *
  *********************************************************************************/
 
