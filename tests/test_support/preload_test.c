581c55f540cdfc62dfdbc4e86e6dafab4d8311b9
Andrew Gontarek <andrewg@cray.com>
2020-02-17 10:57:52 -0600

    Remove definition of weak function get_message
    This wasn't getting overridden like I thought it would when setting
    LD_PRELOAD. Instead just have the symbol be weak and undefinined. We
    will override it via LD_PRELOAD.

--- tests/test_support/preload_test.c
+++ tests/test_support/preload_test.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-__attribute__ ((weak)) char const* get_message() {
-    return "In weak function!";
-}
+char const* get_message() __attribute__ ((weak));
 
 int main(int argc, char** argv) {
+    if (!get_message) {
+        printf("Missing linkage to weak symbol!");
+        return 1;
+    }
+
     printf("%s", get_message());
     return 0;
 }
