c0ef5f7dc99837a53f2c3764d8889c8666f5e9b2
Andrew Gontarek <andrewg@cray.com>
2020-04-06 17:07:44 -0500

    Enable DST powertools repo

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -35,9 +35,10 @@ if [[ "$target_pm" == "$cdst_pm_zypper" ]]; then
     check_exit_status
 elif [[ "$target_pm" == "$cdst_pm_yum" ]]; then
     # Install yum based components
-    yum info epel-release
+    # Note the following will be different on build VMs vs DST. Errors are okay.
     yum config-manager --set-enabled PowerTools
-    yum --assumeyes --enablerepo=PowerTools install \
+    yum config-manager --set-enabled dst-remote-centos8-PowerTools
+    yum --assumeyes install \
         autoconf \
         autoconf-archive \
         automake \
