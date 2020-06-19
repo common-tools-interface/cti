2838d2f43fe064b95ab6c774d483e207a75900e6
Andrew D'Angelo <adangelo@cray.com>
2020-06-19 14:01:34 -0500

    comment cti_websocket

--- src/useful/cti_websocket.hpp
+++ src/useful/cti_websocket.hpp
@@ -38,10 +38,6 @@
 #include <future>
 #include <type_traits>
 
-// // Boost JSON
-// #include <boost/property_tree/ptree.hpp>
-// #include <boost/property_tree/json_parser.hpp>
-
 // Boost WebSocket
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
@@ -65,9 +61,13 @@ namespace cti
 namespace
 {
 
+// Load the proper gateway / API certificates into the SSL context for a stream
 template <typename SslCtx>
 static inline auto set_ssl_certs(SslCtx&& ssl_ctx)
 {
+
+    // Shasta job launch tools do not currently do any certificate verification for SSL,
+    // once that is implemented, we can use the same certificates to perform verification here.
 #if 0
     ssl_ctx.add_certificate_authority(
         boost::asio::buffer(cert.data(), cert.size()), ec);
@@ -81,140 +81,173 @@ static inline auto set_ssl_certs(SslCtx&& ssl_ctx)
 
 }
 
+// Initialize SSL certificates and connect a stream to the given host
 template <typename Ioc, typename SslCtx, typename Stream>
 static inline auto connect_ssl_stream(Ioc&& ioc, SslCtx&& ssl_ctx, Stream&& stream, std::string const& hostname)
 {
+    // Load in gateway / API certificates
     set_ssl_certs(ssl_ctx);
 
+    // Set up the SSL context to connect to the given hostname
     if(!SSL_set_tlsext_host_name(stream.native_handle(), hostname.c_str())) {
         boost::beast::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
         throw boost::beast::system_error{ec};
     }
 
+    // Resolve the hostname and connect the underlying stream
     auto resolver = boost::asio::ip::tcp::resolver{ioc};
     auto const resolver_results = resolver.resolve(hostname, "443");
     boost::beast::get_lowest_layer(stream).connect(resolver_results);
 
+    // Perform SSL handshake
     stream.handshake(boost::asio::ssl::stream_base::client);
 }
 
+// Clean up the stream's connection
 template <typename Stream,
+    // Stream must be moved into function, as it will become invalidated.
+    // Stream&& is a universal reference, so enforce this via type traits
     class = typename std::enable_if<!std::is_lvalue_reference<Stream>::value>::type>
 static inline auto shutdown_ssl_stream(Stream&& stream)
 {
+    // Shut down the underlying socket
     auto ec = boost::beast::error_code{};
     boost::beast::get_lowest_layer(stream).socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
 
+    // Check for shutdown error
     if (ec && (ec != boost::beast::errc::not_connected)) {
         throw boost::beast::system_error{ec};
     }
 }
 
+using TcpStream = boost::beast::ssl_stream<boost::beast::tcp_stream>;
+using WebSocketStream = boost::beast::websocket::stream<boost::beast::ssl_stream<boost::asio::ip::tcp::socket>>;
+
 }
 
-using TcpStream = boost::beast::ssl_stream<boost::beast::tcp_stream>;
-
+// Perform HTTPS GET request to https://<hostname>/<endpoint>, authenticated with <token>
 static inline std::string httpGetReq(std::string const& hostname, std::string const& endpoint, std::string const& token)
 {
+    // Set up SSL stream and connect to host
     auto ioc = boost::asio::io_context{};
     auto ssl_ctx = boost::asio::ssl::context{boost::asio::ssl::context::tlsv12_client};
     auto stream = TcpStream{ioc, ssl_ctx};
     connect_ssl_stream(ioc, ssl_ctx, stream, hostname);
 
+    // Create GET request and fill in required headers
     auto req = boost::beast::http::request<boost::beast::http::string_body>{boost::beast::http::verb::get, endpoint, 11};
     req.set(boost::beast::http::field::host, hostname);
     req.set("Authorization", "Bearer " + token);
     req.set(boost::beast::http::field::user_agent, CTI_RELEASE_VERSION);
-
     req.set(boost::beast::http::field::accept, "application/json");
-    req.prepare_payload();
 
+    // Prepare and write request
+    req.prepare_payload();
     boost::beast::http::write(stream, req);
 
+    // Receive string response
     auto buffer = boost::beast::flat_buffer{};
     auto resp = boost::beast::http::response<boost::beast::http::string_body>{};
-
     boost::beast::http::read(stream, buffer, resp);
 
-    if (resp.base().result_int() == 301) {
-        auto const location = std::string{resp.base()["Location"]};
-        throw std::runtime_error("301 redirect: " + location);
+    // Check HTTP response code
+    if (resp.base().result_int() != 200) {
+        throw std::runtime_error("GET " + endpoint + " failed: " + std::to_string(resp.base().result_int()));
     }
 
+    // Extract result and shut down stream
     auto const result = std::string{resp.body().data()};
-
     shutdown_ssl_stream(std::move(stream));
 
     return result;
 }
 
+// Perform HTTPS DELETE request to https://<hostname>/<endpoint>, authenticated with <token>
 static inline std::string httpDeleteReq(std::string const& hostname, std::string const& endpoint, std::string const& token)
 {
+    // Set up SSL stream and connect to host
     auto ioc = boost::asio::io_context{};
     auto ssl_ctx = boost::asio::ssl::context{boost::asio::ssl::context::tlsv12_client};
     auto stream = TcpStream{ioc, ssl_ctx};
     connect_ssl_stream(ioc, ssl_ctx, stream, hostname);
 
+    // Cretae DELETE request and fill in required headers
     auto req = boost::beast::http::request<boost::beast::http::string_body>{boost::beast::http::verb::delete_, endpoint, 11};
     req.set(boost::beast::http::field::host, hostname);
     req.set("Authorization", "Bearer " + token);
     req.set(boost::beast::http::field::user_agent, CTI_RELEASE_VERSION);
-
     req.set(boost::beast::http::field::accept, "application/json");
-    req.prepare_payload();
 
+    // Prepare and write request
+    req.prepare_payload();
     boost::beast::http::write(stream, req);
 
+    // Receive string response
     auto buffer = boost::beast::flat_buffer{};
     auto resp = boost::beast::http::response<boost::beast::http::string_body>{};
-
     boost::beast::http::read(stream, buffer, resp);
 
-    auto const result = std::string{resp.body().data()};
+    // Check HTTP response code
+    if (resp.base().result_int() != 200) {
+        throw std::runtime_error("DELETE " + endpoint + " failed: " + std::to_string(resp.base().result_int()));
+    }
 
+    // Extract result and shut down stream
+    auto const result = std::string{resp.body().data()};
     shutdown_ssl_stream(std::move(stream));
 
     return result;
 }
 
+// Perform HTTPS POST request to https://<hostname>/<endpoint>, sending JSON <body>
 static inline std::string httpPostJsonReq(std::string const& hostname, std::string const& endpoint, std::string const& token, std::string const& body)
 {
+    // Set up SSL stream and connect to host
     auto ioc = boost::asio::io_context{};
     auto ssl_ctx = boost::asio::ssl::context{boost::asio::ssl::context::tlsv12_client};
     auto stream = TcpStream{ioc, ssl_ctx};
     connect_ssl_stream(ioc, ssl_ctx, stream, hostname);
 
+    // Create string POST request and fill in required headers
     auto req = boost::beast::http::request<boost::beast::http::string_body>{boost::beast::http::verb::post, endpoint, 11};
     req.set(boost::beast::http::field::host, hostname);
     req.set("Authorization", "Bearer " + token);
     req.set(boost::beast::http::field::user_agent, CTI_RELEASE_VERSION);
-
     req.set(boost::beast::http::field::accept, "application/json");
     req.set(boost::beast::http::field::content_type, "application/json");
+
+    // Prepare body and write request
     req.body() = body;
     req.prepare_payload();
-
     boost::beast::http::write(stream, req);
 
+    // Receive string response
     auto buffer = boost::beast::flat_buffer{};
     auto resp = boost::beast::http::response<boost::beast::http::string_body>{};
-
     boost::beast::http::read(stream, buffer, resp);
 
-    auto const result = std::string{resp.body().data()};
+    // Check HTTP response code
+    if (resp.base().result_int() != 200) {
+        throw std::runtime_error("GET " + endpoint + " failed: " + std::to_string(resp.base().result_int()));
+    }
 
+    // Extract result and shut down stream
+    auto const result = std::string{resp.body().data()};
     shutdown_ssl_stream(std::move(stream));
 
     return result;
 }
 
+// Perform HTTPS POST request to https://<hostname>/<endpoint>, sending file at <filePath>
 static inline std::string httpPostFileReq(std::string const& hostname, std::string const& endpoint, std::string const& token, std::string const& filePath)
 {
+    // Set up SSL stream and connect to host
     auto ioc = boost::asio::io_context{};
     auto ssl_ctx = boost::asio::ssl::context{boost::asio::ssl::context::tlsv12_client};
     auto stream = TcpStream{ioc, ssl_ctx};
     connect_ssl_stream(ioc, ssl_ctx, stream, hostname);
 
+    // Create file POST request and fill in required headers
     auto req = boost::beast::http::request<boost::beast::http::file_body>{boost::beast::http::verb::post, endpoint, 11};
     req.set(boost::beast::http::field::host, hostname);
     req.set("Authorization", "Bearer " + token);
@@ -222,49 +255,53 @@ static inline std::string httpPostFileReq(std::string const& hostname, std::stri
     req.set(boost::beast::http::field::accept, "application/json");
     req.set(boost::beast::http::field::content_type, "application/octet-stream");
 
+    // Read file into body and write request
     auto ec = boost::beast::error_code{};
     req.body().open(filePath.c_str(), boost::beast::file_mode::read, ec);
     if (ec) {
         throw boost::beast::system_error{ec};
     }
     req.prepare_payload();
-
     boost::beast::http::write(stream, req);
 
+    // Receive string response
     auto buffer = boost::beast::flat_buffer{};
     auto resp = boost::beast::http::response<boost::beast::http::string_body>{};
-
     boost::beast::http::read(stream, buffer, resp);
 
-    if (resp.base().result_int() == 301) {
-        auto const location = std::string{resp.base()["Location"]};
-        throw std::runtime_error("301 redirect: " + location);
+    // Check HTTP response code
+    if (resp.base().result_int() != 200) {
+        throw std::runtime_error("GET " + endpoint + " failed: " + std::to_string(resp.base().result_int()));
     }
 
+    // Extract result and shut down stream
     auto const result = std::string{resp.body().data()};
-
     shutdown_ssl_stream(std::move(stream));
 
     return result;
 }
 
-using WebSocketStream = boost::beast::websocket::stream<boost::beast::ssl_stream<boost::asio::ip::tcp::socket>>;
-
+// Open persistent Websocket stream at wss://<hostname>:<port>, caller must connect to endpoint
 template <typename Ioc, typename SslCtx>
 static inline auto make_WebSocketStream(Ioc&& ioc, SslCtx&& ssl_ctx,
     std::string const& hostname, std::string const& port,
     std::string const& token)
 {
+    // Load in gateway / API certificates
     set_ssl_certs(ssl_ctx);
 
+    // Initialize stream
     auto result = WebSocketStream{ioc, ssl_ctx};
 
+    // Resolve hostname and connect asynchronously
     auto resolver = boost::asio::ip::tcp::resolver{ioc};
     auto const resolver_results = resolver.resolve(hostname, port);
     boost::asio::connect(boost::beast::get_lowest_layer(result), resolver_results.begin(), resolver_results.end());
 
+    // Peform SSL handshake
     result.next_layer().handshake(boost::asio::ssl::stream_base::client);
 
+    // Set required headers
     result.set_option(boost::beast::websocket::stream_base::decorator(
         [hostname, token](boost::beast::websocket::request_type& req) {
             req.set(boost::beast::http::field::host, hostname);
@@ -278,6 +315,8 @@ static inline auto make_WebSocketStream(Ioc&& ioc, SslCtx&& ssl_ctx,
     return result;
 }
 
+// Helper to write strings produced by <dataSource> to <webSocketStream>
+// Can be paired with std::future / std::async for asynchronous input to a Websocket
 template <typename Func>
 static inline void webSocketInputTask(WebSocketStream& webSocketStream, Func&& dataSource)
 {
@@ -307,6 +346,8 @@ static inline void webSocketInputTask(WebSocketStream& webSocketStream, Func&& d
     }
 };
 
+// Helper to write strings produced by <webSocketStream> to <dataSink>
+// Can be paired with std::future / std::async for asynchronous output from a Websocket
 template <typename Func>
 static inline void webSocketOutputTask(WebSocketStream& webSocketStream, Func&& dataSink)
 {
@@ -347,6 +388,7 @@ static inline void webSocketOutputTask(WebSocketStream& webSocketStream, Func&& 
     }
 }
 
+// Synchronously read a single string from <webSocketStream>
 static inline auto webSocketReadString(WebSocketStream& webSocketStream)
 {
     // Set up buffers
