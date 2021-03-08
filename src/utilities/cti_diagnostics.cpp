add6499571d4933ce897c06d92af0fd1f1f3bb83
Andrew D'Angelo <adangelo@cray.com>
2021-03-08 15:15:34 -0600

    Stub out CTI diagnostics frontend and backend utilities

---
+++ src/utilities/cti_diagnostics.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************\
+ * cti_diagnostics.cpp - Diagnose CTI launch process
+ *
+ * Copyright 2021 Hewlett Packard Enterprise Development LP.
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common_tools_fe.h"
+
+int main(int argc, char **argv)
+{
+    (void)cti_launchApp(nullptr, -1, -1, nullptr, nullptr, nullptr);
+
+    return 0;
+}
