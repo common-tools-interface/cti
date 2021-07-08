b071ee4927accc1dddbba3c2721a42fb342c3781
Andrew D'Angelo <adangelo@cray.com>
2021-07-07 19:49:23 -0500

    Allow input files to be pipes, add stdin FD launch function variant tests

--- tests/function/bin/cti_fe_function_test.cpp
+++ tests/function/bin/cti_fe_function_test.cpp
@@ -534,6 +534,50 @@ TEST_F(CTIFEFunctionTest, InputFile) {
     close(pipes[1]);
 }
 
+// Test that an app can read input from file descriptor
+TEST_F(CTIFEFunctionTest, StdinPipe) {
+    // set up string contents
+    auto const echoString = std::to_string(getpid()) + "\n";
+
+    int stdin_pipe[2];
+    int stdout_pipe[2];
+    int r = 0;
+
+    r = pipe(stdin_pipe);
+    ASSERT_EQ(r, 0) << "Failed to create a pipe.";
+    r = pipe(stdout_pipe);
+    ASSERT_EQ(r, 0) << "Failed to create a pipe.";
+
+    FILE *job_stdout = fdopen(stdout_pipe[0], "r");
+    ASSERT_NE(job_stdout, nullptr) << "Failed to open pipe for reading.";
+
+    // set up launch arguments
+    std::vector<std::string> argv = createSystemArgv({"../src/mpi_wrapper", "/usr/bin/cat"});
+    auto const  stdoutFd = stdout_pipe[1];
+    auto const  stderrFd = -1;
+    auto const  stdinFd  = stdin_pipe[0];
+    char const* chdirPath = nullptr;
+    char const* const* envList  = nullptr;
+
+    // launch app
+    auto const appId = watchApp(cti_launchAppBarrier_fd(cstrVector(argv).data(), stdoutFd, stderrFd, stdinFd, chdirPath, envList));
+    ASSERT_GT(appId, 0) << cti_error_str();
+    ASSERT_EQ(cti_appIsValid(appId), true) << cti_error_str();
+
+    ASSERT_EQ(cti_releaseAppBarrier(appId), SUCCESS) << cti_error_str();
+
+    // write app input
+    write(stdin_pipe[1], echoString.c_str(), echoString.length() + 1);
+    close(stdin_pipe[1]);
+
+    // get app output
+    char buf[64];
+    memset(buf, '\0', 64);
+
+    ASSERT_NE(fgets(buf, sizeof(buf) - 1, job_stdout), nullptr) << "Failed to read app output from pipe.";
+    ASSERT_EQ(std::string(buf), echoString);
+}
+
 // // Test that an app can forward environment variables
 TEST_F(CTIFEFunctionTest, EnvVars) {
     // set up string contents
