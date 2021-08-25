401ef26855d5ebcf22365b69db50b710140d6af5
Andrew D'Angelo <adangelo@cray.com>
2021-08-25 13:10:01 -0500

    Add unit test file for Flux API parsing

---
+++ tests/unit/flux_api_parse_tests.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************\
+ * flux_api_parse_tests.cpp - Flux API response parsing tests
+ *
+ * Copyright 2020 Hewlett Packard Enterprise Development LP.
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ******************************************************************************/
+
+#include "cti_defs.h"
+
+#include "src/frontend/frontend_impl/Flux/Frontend.hpp"
+#include "src/frontend/frontend_impl/Flux/FluxAPI.hpp"
+
+// include google testing files
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using ::testing::Return;
+using ::testing::_;
+using ::testing::Invoke;
+using ::testing::WithoutArgs;
+using ::testing::EndsWith;
+
+TEST(FluxApiParse, First)
+{
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
