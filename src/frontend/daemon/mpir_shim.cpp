aa4dffb32c5fbef67a48854497d7c1ad9636559a
Josh Chandler <josh.chandler@hpe.com>
2021-09-10 15:20:33 +0000

    better handling of debug output in MPIR shim

--- src/frontend/daemon/mpir_shim.cpp
+++ src/frontend/daemon/mpir_shim.cpp
@@ -29,6 +29,7 @@
 
 #include <unistd.h>
 #include <limits.h>
+#include <stdarg.h>
 
 #include <signal.h>
 
@@ -43,13 +44,35 @@
 
 static const auto shimToken = std::string{CTI_SHIM_TOKEN};
 
+static bool debug_enabled() {
+	static const auto _enabled = []() {
+		if (::getenv("CTI_DEBUG")) {
+			return true;
+		}
+
+		return false;
+	}();
+	return _enabled;
+}
+
+static void log(const char* format, ...)
+{
+	if (debug_enabled()) {
+		va_list argptr;
+		va_start(argptr, format);
+		vfprintf(stderr, format, argptr);
+		va_end(argptr);
+	}
+}
+
 /*
  * MPIR shim is used by bootstrap/wrapper scripts that aren't MPIR compliant.
  * CTI will insert the shim into PATH with the same name as the normal launcher,
  * and the wrapper will launch the shim instead of the actual launcher. The shim
  * then starts the real launcher in a stopped state, sends the pid to CTI, which
  * then attaches and reads the MPIR info. The shim only activates if the
- * CTI_SHIM_TOKEN is the last argument on the command line.
+ * CTI_SHIM_TOKEN is the last argument on the command line, and the token is not
+ * passed to the actual job launch.
 */
 
 static auto parse_from_env(int argc, char const* argv[])
@@ -86,7 +109,7 @@ static auto parse_from_env(int argc, char const* argv[])
 	}
 
 	// Check for the shim activation token; some wrappers make their own calls
-	// to srun and we should only activate the shim on the true job launch.
+	// to the job launcher and we should only activate the shim on the true job launch.
 	// The token will always be the last argument if it is present.
 	if (argv[argc - 1] == shimToken) {
 		hasShimToken = true;
@@ -110,7 +133,7 @@ int main(int argc, char const* argv[], char const* env[])
 	}
 
 	if (hasShimToken) {
-		fprintf(stderr, "has token\n");
+		log("cti shim token detected\n");
 
 		// Close unused pipe end
 		::close(inputFd);
@@ -124,59 +147,59 @@ int main(int argc, char const* argv[], char const* env[])
 			::unsetenv(env_var);
 		}
 		if (::setenv("PATH", originalPath.c_str(), 1)) {
-			fprintf(stderr, "failed to restore PATH: %s\n", strerror(errno));
+			log("failed to restore PATH: %s\n", strerror(errno));
 			exit(-1);
 		}
 
-		fprintf(stderr, "launching\n");
-
 		// Create MPIR launch instance based on arguments
-		fprintf(stderr, "launcher: %s\n", launcherPath.c_str());
-		fprintf(stderr, "argv:\n");
+		log("launcher: %s\n", launcherPath.c_str());
+		log("argv:\n");
 		for (const auto arg : launcherArgv) {
-			fprintf(stderr, "\t%s\n", arg.c_str());
+			log("\t%s\n", arg.c_str());
 		}
 
-		pid_t pid;
-		{
-			// Start job launcher in stopped state
+		// Start job launcher in stopped state and get the pid
+		auto pid = [](const std::string &launcherPath, const std::vector<std::string> &launcherArgv){
 			auto launcherProcess = Inferior{launcherPath, launcherArgv, {}, {}};
 
-			pid = launcherProcess.getPid();
-			fprintf(stderr, "launcher pid reports %d\n", pid);
+			auto pid = launcherProcess.getPid();
+			log("launcher pid is %d\n", pid);
 
 			// Ensure inferior stays stopped on detach
 			::kill(pid, SIGSTOP);
 
 			// Inferior will detach on scope exit
-		}
+			return pid;
+		}(launcherPath, launcherArgv);
 
-		// Send attach info to CTI
-		fprintf(stderr, "detached\n");
-		fprintf(stderr, "sending data\n");
-
-		// send PID
+		// Send PID to CTI
+		log("detached, sending pid\n");
 		rawWriteLoop(outputFd, pid);
 
 		// Close pipe
-		if (outputFd != 1) {
-			::close(outputFd);
-		}
+		::close(outputFd);
 
 		// It is now the job of CTI to read MPIR data and send SIGCONT to the
 		// inferior launcher when it is done reading.
 
-		// stay alive until launcher is done
-		fprintf(stderr, "waiting for child\n");
+		// Stay alive until launcher is done so wrapper scripts that monitor
+		// the lifetime of their "launcher" don't exit immediately
+		log("waiting for child\n");
 		waitpid(pid, nullptr, 0);
+		log("child exited\n");
 	} else {
-		fprintf(stderr, "no token, forwarding to %s\n", launcherPath.c_str());
+		log("no token, forwarding to %s\n", launcherPath.c_str());
 		argv[0] = launcherPath.c_str();
+
+		log("\t");
+		for (int i = 0; i < argc; i++) {
+			log("%s ", argv[i]);
+		}
+		log("\n");
+
 		execvpe(launcherPath.c_str(), const_cast<char* const*>(argv), const_cast<char* const*>(env));
 		return -1;
 	}
-
-	fprintf(stderr, "done\n");
 
 	return 0;
 }
