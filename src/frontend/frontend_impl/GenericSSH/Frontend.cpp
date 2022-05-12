6ca24ed3d043c089c4c9f102f3c437a24ba427e8
John Vogt <john.leroy.vogt@hpe.com>
2022-05-12 16:24:41 -0400

    update copyright from previous fix

--- src/frontend/frontend_impl/GenericSSH/Frontend.cpp
+++ src/frontend/frontend_impl/GenericSSH/Frontend.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************\
  * Frontend.cpp -  Frontend library functions for SSH based workload manager.
  *
- * Copyright 2017-2020 Hewlett Packard Enterprise Development LP.
+ * Copyright 2017-2022 Hewlett Packard Enterprise Development LP.
  *
  *     Redistribution and use in source and binary forms, with or
  *     without modification, are permitted provided that the following
