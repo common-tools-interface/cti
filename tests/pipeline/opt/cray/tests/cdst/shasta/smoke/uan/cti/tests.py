ae478b980c15fd713ca3add154eae109b510d1db
Josh Chandler <josh.chandler@hpe.com>
2024-01-18 11:26:54 -0600

    ex: PE-52009: Update pipeline tests (#541)

--- tests/pipeline/opt/cray/tests/cdst/shasta/smoke/uan/cti/tests.py
+++ tests/pipeline/opt/cray/tests/cdst/shasta/smoke/uan/cti/tests.py
@@ -8,13 +8,21 @@ if os.environ.get("TEST_BASE_DIR") == None:
 sys.path.append(os.environ.get("TEST_BASE_DIR") + "/opt/cray/tests/cdst/resources/cti/cdst-test")
 import cdst_test.modules as mods
 
+def require_modules(test):
+    if mods.MODULES_HOME == None:
+        test.cancel(f"precondition failed: module command not available (no modules home)")
+    if mods.MODULES_FLAVOR == None:
+        test.cancel(f"precondition failed: module command not available (couldn't detect modules flavor lua/tcl/etc)")
+
 def require_module_not_loaded(test, module):
+    require_modules(test)
     loaded_modules = mods.list()
     for fullname in loaded_modules:
         if (module + "/") in fullname:
             test.cancel(f"precondition failed: {module} is already loaded ({fullname})")
 
 def require_module_available(test, module):
+    require_modules(test)
     if not mods.dry_run("load", module):
         test.cancel(f"precondition failed: {module} is not available")
 
@@ -22,7 +30,7 @@ def require_zero_exit_code(test, command):
     try:
         result = ac.utils.process.run(command)
     except Exception as e:
-        # an exception is raised here if pkg-config doesn't exist or if it
+        # an exception is raised here if the command doesn't exist or if it
         # exits with non-zero. we catch it because want that to show up as a
         # CANCEL, not an ERROR
         test.cancel(f"precondition failed: {command} failed {e})")
@@ -43,7 +51,7 @@ class CtiModules(ac.Test):
     def test_module_load(self):
         """
         test that we can load the cray-cti module.
-        
+
         preconditions:
         1. the cray-cti module is not loaded
         2. the cray-cti module is available
@@ -55,79 +63,6 @@ class CtiModules(ac.Test):
 
         loaded_modules_no_versions = [m[:m.find('/')] for m in mods.list()]
         self.assertTrue("cray-cti" in loaded_modules_no_versions, "cray-cti missing from module list")
-
-    def test_module_load_devel(self):
-        """
-        test that we can load the cray-cti-devel module and that it correctly loads the cray-cti module.
-        
-        preconditions:
-        1. the cray-cti-devel and cray-cti modules are not loaded
-        2. the cray-cti-devel and cray-cti modules are available
-        """
-        require_module_not_loaded(self, "cray-cti")
-        require_module_not_loaded(self, "cray-cti-devel")
-        require_module_available(self, "cray-cti")
-        require_module_available(self, "cray-cti-devel")
-
-        self.assertTrue(mods.invoke_successfully("load", "cray-cti-devel"), "`module load cray-cti-devel` failed")
-
-        loaded_modules_no_versions = [m[:m.find('/')] for m in mods.list()]
-        self.assertTrue("cray-cti-devel" in loaded_modules_no_versions, "cray-cti-devel missing from module list")
-        self.assertTrue("cray-cti" in loaded_modules_no_versions, "cray-cti missing from module list")
-
-        loaded_cti_modules = mods.list("cray-cti")
-        version = None
-        for mod in loaded_cti_modules:
-            if "cray-cti-devel" in mod:
-                version = mod[mod.find("/"):]
-
-        self.assertTrue(f"cray-cti-devel{version}" in mods.list(), f"cray-cti-devel{version} missing from module list")
-        self.assertTrue(f"cray-cti{version}" in mods.list(), f"cray-cti{version} missing from module list")
-
-    def test_devel_pkg_config(self):
-        """
-        test that loading cray-cti-devel exposes pkg-config functionality
-        
-        preconditions:
-        1. pkg-config is present
-        2. the cray-cti-devel module is not loaded
-        3. the cray-cti-devel module is available
-        4. pkg-config common_tools_fe returns nothing
-        """
-        require_pkg_config(self)
-        require_module_not_loaded(self, "cray-cti-devel")
-        require_module_available(self, "cray-cti-devel")
-        if ac.utils.process.run("pkg-config common_tools_fe", ignore_status=True).exit_status == 0:
-            self.cancel("pkg-config common_tools_fe worked before the test")
-
-        self.assertTrue(mods.invoke_successfully("load", "cray-cti-devel"), "`module load cray-cti-devel` failed")
-        self.assertTrue(ac.utils.process.run("pkg-config common_tools_fe", ignore_status=True).exit_status == 0, "`pkg-config common_tools_fe` failed after loading cray-cti-devel")
-        self.assertTrue(ac.utils.process.run("pkg-config common_tools_be", ignore_status=True).exit_status == 0, "`pkg-config common_tools_be` failed after loading cray-cti-devel")
-
-    def test_build(self):
-        """
-        test that loading cray-cti-devel lets us build a cti app
-
-        preconditions:
-        1. pkg-config is present
-        2. cc and make are present
-        3. the cray-cti-devel module is not loaded
-        4. the cray-cti-devel module is available
-        5. pkg-config common_tools_fe returns nothing
-        """
-        require_pkg_config(self)
-        require_build_tools(self)
-        require_module_not_loaded(self, "cray-cti-devel")
-        require_module_available(self, "cray-cti-devel")
-        if ac.utils.process.run("pkg-config common_tools_fe", ignore_status=True).exit_status == 0:
-            self.cancel("pkg-config common_tools_fe worked before the test")
-
-        resources_dir = os.environ.get("TEST_BASE_DIR") + "/opt/cray/tests/cdst/resources/cti/smoke"
-
-        os.chdir(resources_dir)
-        self.assertTrue(mods.invoke_successfully("load", "cray-cti-devel"), "`module load cray-cti-devel` failed")
-        self.assertTrue(ac.utils.process.run("make test_build", ignore_status=True).exit_status == 0, "`make test_build` failed")
-        self.assertTrue(ac.utils.process.run("./bin/test_build", ignore_status=True).exit_status == 0, "`./bin/test_build` failed")
 
 class CtiPrebuilt(ac.Test):
     def setUp(self):
